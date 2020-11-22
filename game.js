08e654a77ab49ced442aa2967ba8f0f5d3b2de8c
Ayoade Adenuga <adenugaayoadehenry63@gmail.com>
2020-11-22 14:08:40 +0100

    at svg inline attr bug

--- game.js
+++ game.js
@@ -1,3 +1,4 @@
+
 function testAnim(x) {
   $('.modal .modal-dialog').attr('class', 'modal-dialog  ' + x + '  animated');
 };
@@ -9,55 +10,146 @@ $('#myModal').on('hide.bs.modal', function (e) {
   $('.modal .modal-dialog').attr('class', 'modal-dialog slideOutDown animated');
 })
 
+let yourScore = 0, computerScore = 0;
+
 
 $(".paper.icon").on("click", function() {
-  $(".scissors.icon, .rock.icon, .paper.icon").off()
-  $(".paper.icon").fadeOut(500).fadeIn(500)
-  $(".first-svg, .rock.icon, .low-text, .second-rock").toggleClass("invisible");
-  $(".scissors.icon").addClass("house-color");
-  $(".second-scissors").toggleClass("invisible");
-  setTimeout(function(){
-    housePick();
-  },3000);
+  processPaper();
 })
 
 $(".scissors.icon").on("click", function(){
-  yourPick("scissors");
-  setTimeout(function(){
-    housePick();
-  },3000);
+  process("scissors");
+})
+$(".rock.icon").on("click", function(){
+  process("rock")
 })
 
-$(".rock.icon").on("click", function(){
-  yourPick("rock");
+
+function process(arg) {
+  let yourChosen = yourPick(arg);
   setTimeout(function(){
-    housePick();
+    let computerChosen = housePick();
+    check(computerChosen, yourChosen);
+    setTimeout(function(){
+      $(".second-" + computerChosen).addClass("hide");
+      $(".second-scissors").removeClass("hide invisiblility")
+      $(".scissors.falseIcon").toggleClass(computerChosen + "-span");
+      if (yourChosen != "paper"){
+        $(".first-" + yourChosen).addClass("hide");
+        $("#first-paper").attr("id", "hide");
+      }
+      $(".house-text").toggleClass("invisiblility");
+      $(".paper.falseIcon").toggleClass(yourChosen + "-span");
+      $(".first-svg, .rock.falseIcon, .low-text").toggleClass("invisiblility");
+      $(".scissors.falseIcon, .rock.falseIcon, .paper.falseIcon").toggleClass("icon").toggleClass("falseIcon")
+    },2500)
   },3000);
-})
+};
+
+function processPaper(){
+  $(".scissors.icon, .rock.icon, .paper.icon").toggleClass("icon").toggleClass("falseIcon")
+  $(".paper.falseIcon").fadeOut(500).fadeIn(500)
+  $(".first-svg, .rock.falseIcon, .low-text, .second-rock").toggleClass("invisiblility");
+  $(".scissors.falseIcon").addClass("house-color");
+  $(".second-scissors").toggleClass("invisiblility");
+  let yourChosen = "paper"
+  setTimeout(function(){
+    let computerChosen = housePick();
+    check(computerChosen, yourChosen)
+    setTimeout(function(){
+      $(".second-" + computerChosen).addClass("hide");
+      $(".second-scissors").removeClass("hide invisiblility")
+      $(".scissors.falseIcon").toggleClass(computerChosen + "-span");
+      if (yourChosen != "paper"){
+        $(".first-" + yourChosen).addClass("hide");
+        $("#first-paper").attr("id", "hide");
+      }
+      $(".house-text").toggleClass("invisiblility");
+      $(".paper.falseIcon").toggleClass(yourChosen + "-span");
+      $(".first-svg, .rock.falseIcon, .low-text").toggleClass("invisiblility");
+      $(".scissors.falseIcon, .rock.falseIcon, .paper.falseIcon").toggleClass("icon").toggleClass("falseIcon")
+    },2500)
+  },3000);
+}
+
+function check(computerChosen, yourChosen) {
+  setTimeout(function() {
+    if (computerChosen === "scissors" && yourChosen === "rock"){
+      yourScore += 1;
+      $("#you").text(yourScore);
+    } else if(computerChosen === "rock" && yourChosen === "paper"){
+      yourScore += 1;
+      $("#you").text(yourScore);
+    } else if (computerChosen === "paper" && yourChosen === "scissors"){
+      yourScore += 1;
+      $("#you").text(yourScore);
+    } else if (yourChosen === "scissors" && computerChosen === "rock"){
+      computerScore += 1;
+      $("#computer").text(computerScore);
+    } else if(yourChosen === "rock" && computerChosen === "paper"){
+      computerScore += 1;
+      $("#computer").text(computerScore);
+    } else if (yourChosen === "paper" && computerChosen === "scissors"){
+      computerScore += 1;
+      $("#computer").text(computerScore);
+    }
+  },1000)
+}
 
 function housePick() {
   let randNum = (Math.floor(Math.random() * 3));
   let svg = ["paper", "scissors", "rock"]
 
-  $(".scissors.icon").toggleClass("house-color");
+  $(".scissors.falseIcon").toggleClass("house-color");
   if (svg[randNum] === "scissors"){
-    $(".scissors.icon").fadeOut(500).fadeIn(500);
-    $(".second-scissors").toggleClass("invisible");
+    $(".scissors.falseIcon").toggleClass(svg[randNum] + "-span");
+    $(".second-scissors").toggleClass("invisiblility");
   } else {
-    $(".scissors.icon").addClass("." + svg[randNum] + "-spam");
-    $(".second-scissors").addClass("hide")
+    $(".scissors.falseIcon").toggleClass(svg[randNum] + "-span");
+    $(".second-scissors").toggleClass("hide")
     $(".second-" + svg[randNum]).toggleClass("hide")
   }
+  $(".house-text").toggleClass("invisiblility");
+
+  return svg[randNum];
 }
 
 
 
 function yourPick(icon) {
-  $(".scissors.icon, .rock.icon, .paper.icon").off()
-  $(".first-paper").fadeOut(2000).addClass("hide");
-  $(".paper.icon").addClass(icon + "-span");
+  $(".scissors.icon, .rock.icon, .paper.icon").toggleClass("icon").toggleClass("falseIcon")
+  $(".first-paper").fadeOut(2000).toggleClass("hide");
+  $(".paper.falseIcon").toggleClass(icon + "-span");
   $(".first-" + icon).fadeIn(2000).toggleClass("hide");
-  $(".first-svg, .rock.icon, .low-text").toggleClass("invisible");
-  $(".scissors.icon").toggleClass("house-color");
-  $(".second-scissors").toggleClass("invisible");
+  $(".first-svg, .rock.falseIcon, .low-text").toggleClass("invisiblility");
+  $(".scissors.falseIcon").toggleClass("house-color");
+  $(".second-scissors").toggleClass("invisiblility");
+  return icon;
 }
+
+// $(".paper.icon").on("click", function() {
+//   paperListner()
+// })
+// function paperListner() {
+//   $(".paper.icon").on("click", function() {
+//     processPaper();
+//   })
+// }
+//
+// $(".scissors.icon").on("click", function(){
+//   scissorsListner()
+// })
+// function scissorsListner() {
+//   $(".scissors.icon").on("click", function(){
+//     process("scissors");
+//   })
+// }
+//
+// $(".rock.icon").on("click", function(){
+//   rockListner();
+// })
+// function rockListner() {
+//   $(".rock.icon").on("click", function(){
+//     process("rock")
+//   })
+// }
