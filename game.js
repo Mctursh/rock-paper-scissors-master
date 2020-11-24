bc49e9a92ededbec11c000ae3015b44eb48a854d
Ayoade Adenuga <adenugaayoadehenry63@gmail.com>
2020-11-24 18:20:52 +0100

    completed but remains minor ending display feature

--- game.js
+++ game.js
@@ -1,67 +1,125 @@
-
 function testAnim(x) {
   $('.modal .modal-dialog').attr('class', 'modal-dialog  ' + x + '  animated');
 };
-$('#myModal').on('show.bs.modal', function (e) {
+$('#myModal').on('show.bs.modal', function(e) {
   $('.modal .modal-dialog').attr('class', 'modal-dialog slideInUp animated');
 })
 
-$('#myModal').on('hide.bs.modal', function (e) {
+$('#myModal').on('hide.bs.modal', function(e) {
   $('.modal .modal-dialog').attr('class', 'modal-dialog slideOutDown animated');
 })
 
-let yourScore = 0, computerScore = 0;
 
 
 $(".paper.icon").on("click", function() {
-  processPaper();
+  process("paper");
 })
 
-$(".scissors.icon").on("click", function(){
+$(".scissors.icon").on("click", function() {
   process("scissors");
 })
-$(".rock.icon").on("click", function(){
+$(".rock.icon").on("click", function() {
   process("rock")
 })
 
+$("button.btn-light").on("click", function() {
+  $(".house-text, .low-text").toggleClass("invisiblility");
+  $(".bottom").toggleClass("invisiblility")
+  $(".first-svg, .rock.icon").toggleClass("invisiblility");
+  $("#you, #computer").text(0)
+  yourScore = 0;
+  computerScore = 0;
+  $(".paper.icon").on("click", function() {
+    process("paper");
+  })
+
+  $(".scissors.icon").on("click", function() {
+    process("scissors");
+  })
+  $(".rock.icon").on("click", function() {
+    process("rock")
+  })
+})
+
+
+let yourScore = 0;
+let computerScore = 0;
+
+
+
 
 function process(arg) {
-  let yourChosen = yourPick(arg);
-  setTimeout(function(){
-    let computerChosen = housePick();
-    check(computerChosen, yourChosen);
-    setTimeout(function(){
-      $(".second-" + computerChosen).addClass("hide");
-      $(".second-scissors").removeClass("hide invisiblility")
-      $(".scissors.falseIcon").toggleClass(computerChosen + "-span");
-      if (yourChosen != "paper"){
-        $(".first-" + yourChosen).attr("style", "display: none !important;");
-        $(".first-paper").attr("style", "display: inline !important;");
-      }
+  if (arg === "paper") {
+    processPaper()
+  } else {
+    let yourChosen = yourPick(arg);
+    setTimeout(function() {
+      let computerChosen = housePick();
+      check(computerChosen, yourChosen);
+      setTimeout(function() {
 
-      $(".house-text").toggleClass("invisiblility");
-      $(".paper.falseIcon").toggleClass(yourChosen + "-span");
-      $(".first-svg, .rock.falseIcon, .low-text").toggleClass("invisiblility");
-      $(".scissors.falseIcon, .rock.falseIcon, .paper.falseIcon").toggleClass("icon").toggleClass("falseIcon")
-    },2500)
-  },3000);
-};
+          $(".second-" + computerChosen).addClass("hide");
+          $(".second-scissors").removeClass("hide invisiblility")
+          $(".scissors.falseIcon").toggleClass(computerChosen + "-span");
+          if (yourChosen != "paper") {
+            $(".first-" + yourChosen).attr("style", "display: none !important;");
+            $(".first-paper").attr("style", "display: inline !important;");
+          }
 
-function processPaper(){
-  $(".scissors.icon, .rock.icon, .paper.icon").toggleClass("icon").toggleClass("falseIcon")
+          $(".house-text").toggleClass("invisiblility");
+          $(".paper.falseIcon").toggleClass(yourChosen + "-span");
+          $(".first-svg, .rock.falseIcon, .low-text").toggleClass("invisiblility");
+          $(".scissors.falseIcon, .rock.falseIcon, .paper.falseIcon").toggleClass("icon").toggleClass("falseIcon")
+          if (yourScore < 10) {
+            if (computerScore < 10) {
+              $(".paper.icon").on("click", function() {
+                process("paper");
+              })
+              $(".scissors.icon").on("click", function() {
+                process("scissors");
+              })
+              $(".rock.icon").on("click", function() {
+                process("rock")
+              })
+            } else {
+              $(".scissors.icon, .rock.icon, .paper.icon").off()
+              $(".house-text, .low-text").toggleClass("invisiblility");
+              if (yourScore > computerScore) {
+                winnerDisplay()
+              } else {
+                loserDisplay()
+              }
+            }
+          } else {
+            $(".scissors.icon, .rock.icon, .paper.icon").off()
+            $(".house-text, .low-text").toggleClass("invisiblility");
+            if (yourScore > computerScore) {
+              winnerDisplay()
+            } else {
+              loserDisplay()
+            }
+        }
+      },2500)
+    }, 3000)}
+  }
+
+
+
+function processPaper() {
+  $(".scissors.icon, .rock.icon, .paper.icon").toggleClass("icon").toggleClass("falseIcon").off();
   $(".paper.falseIcon").fadeOut(500).fadeIn(500)
   $(".first-svg, .rock.falseIcon, .low-text, .second-rock").toggleClass("invisiblility");
   $(".scissors.falseIcon").addClass("house-color");
   $(".second-scissors").toggleClass("invisiblility");
   let yourChosen = "paper"
-  setTimeout(function(){
+  setTimeout(function() {
     let computerChosen = housePick();
     check(computerChosen, yourChosen)
-    setTimeout(function(){
+    setTimeout(function() {
       $(".second-" + computerChosen).addClass("hide");
       $(".second-scissors").removeClass("hide invisiblility")
       $(".scissors.falseIcon").toggleClass(computerChosen + "-span");
-      if (yourChosen != "paper"){
+      if (yourChosen != "paper") {
         $(".first-" + yourChosen).addClass("hide");
         $("#first-paper").attr("id", "hide");
       }
@@ -69,32 +127,75 @@ function processPaper(){
       $(".paper.falseIcon").toggleClass(yourChosen + "-span");
       $(".first-svg, .rock.falseIcon, .low-text").toggleClass("invisiblility");
       $(".scissors.falseIcon, .rock.falseIcon, .paper.falseIcon").toggleClass("icon").toggleClass("falseIcon")
-    },2500)
-  },3000);
+      if (yourScore < 10) {
+        if (computerScore < 10) {
+          $(".paper.icon").on("click", function() {
+            process("paper");
+          })
+
+          $(".scissors.icon").on("click", function() {
+            process("scissors");
+          })
+          $(".rock.icon").on("click", function() {
+            process("rock")
+          })
+        } else {
+          $(".scissors.icon, .rock.icon, .paper.icon").off()
+          $(".house-text, .low-text").toggleClass("invisiblility");
+          if (yourScore > computerScore) {
+            winnerDisplay()
+          } else {
+            loserDisplay()
+          }
+        }
+      } else {
+        $(".scissors.icon, .rock.icon, .paper.icon").off()
+        $(".house-text, .low-text").toggleClass("invisiblility");
+        if (yourScore > computerScore) {
+          winnerDisplay()
+        } else {
+          loserDisplay()
+        }
+      }
+    }, 2500)
+  }, 3000);
+
+}
+
+
+function winnerDisplay() {
+  $(".first-svg, .rock.icon").toggleClass("invisiblility");
+  $(".bottom").toggleClass("invisiblility")
+}
+
+function loserDisplay() {
+  $(".first-svg, .rock.icon").toggleClass("invisiblility");
+  $(".bottom").toggleClass("invisiblility");
+  $(".bottom h1").text("YOU LOSE")
 }
 
 function check(computerChosen, yourChosen) {
   setTimeout(function() {
-    if (computerChosen === "scissors" && yourChosen === "rock"){
+    if (computerChosen === "scissors" && yourChosen === "rock") {
       yourScore += 1;
       $("#you").text(yourScore);
-    } else if(computerChosen === "rock" && yourChosen === "paper"){
+    } else if (computerChosen === "rock" && yourChosen === "paper") {
       yourScore += 1;
       $("#you").text(yourScore);
-    } else if (computerChosen === "paper" && yourChosen === "scissors"){
+    } else if (computerChosen === "paper" && yourChosen === "scissors") {
       yourScore += 1;
       $("#you").text(yourScore);
-    } else if (yourChosen === "scissors" && computerChosen === "rock"){
+    } else if (yourChosen === "scissors" && computerChosen === "rock") {
       computerScore += 1;
       $("#computer").text(computerScore);
-    } else if(yourChosen === "rock" && computerChosen === "paper"){
+    } else if (yourChosen === "rock" && computerChosen === "paper") {
       computerScore += 1;
       $("#computer").text(computerScore);
-    } else if (yourChosen === "paper" && computerChosen === "scissors"){
+    } else if (yourChosen === "paper" && computerChosen === "scissors") {
       computerScore += 1;
       $("#computer").text(computerScore);
     }
-  },1000)
+  }, 1000)
 }
 
 function housePick() {
@@ -102,7 +203,7 @@ function housePick() {
   let svg = ["paper", "scissors", "rock"]
 
   $(".scissors.falseIcon").toggleClass("house-color");
-  if (svg[randNum] === "scissors"){
+  if (svg[randNum] === "scissors") {
     $(".scissors.falseIcon").toggleClass(svg[randNum] + "-span");
     $(".second-scissors").toggleClass("invisiblility");
   } else {
@@ -118,7 +219,7 @@ function housePick() {
 
 
 function yourPick(icon) {
-  $(".scissors.icon, .rock.icon, .paper.icon").toggleClass("icon").toggleClass("falseIcon")
+  $(".scissors.icon, .rock.icon, .paper.icon").toggleClass("icon").toggleClass("falseIcon").off();
   $(".first-paper").fadeOut(2000).attr("style", "display: none !important;");
   $(".paper.falseIcon").toggleClass(icon + "-span");
   $(".first-" + icon).fadeIn(2000).attr("style", "display: inline !important;");
